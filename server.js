945c53af9835dcabd74946da8f2175e650405161
psurer <surereker@icloud.com>
2021-08-23 14:33:42 -0400

    setting up apollo server

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
-const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./server/schemas');
+const { authMiddleware } = require('./server/utils/auth');
+const db = require('./server/config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,18 +14,16 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-server.applyMiddleware({ app });
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, './client/build')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 db.once('open', () => {
@@ -33,4 +31,11 @@ db.once('open', () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
+});
+
+
+// The `listen` method launches a web server.
+
+server.start().then(({ url }) => {
+  server.applyMiddleware({ app });
 });
